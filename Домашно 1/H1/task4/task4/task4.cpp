d34372e05072cc2678b2f7dcc5c5ebc04712c4d1
Lyubo11 <99593439+Lyubo11@users.noreply.github.com>
2023-11-12 19:05:55 +0200

    Add files via upload

---
+++ Домашно 1/H1/task4/task4/task4.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int generateSecretNumber() {
+    int secretNumber = 0;
+    int digits[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    for (int i = 0; i < 4; ++i) {
+        int index = rand() % (9 - i);
+        secretNumber = secretNumber * 10 + digits[index];
+        swap(digits[index], digits[8 - i]);
+    }
+
+    return secretNumber;
+}
+
+void checkGuess(int secretNumber, int guess, int& bulls, int& cows) {
+    bulls = 0;
+    cows = 0;
+
+    for (int i = 0; i < 4; ++i) {
+        int secretDigit = secretNumber % 10;
+        secretNumber = secretNumber / 10;
+        int guessDigit = guess % 10;
+        guess = guess / 10;
+
+        if (secretDigit == guessDigit) {
+            ++bulls;
+        } else if ((secretNumber / 1000) == (guess % 10) || ((secretNumber / 100) % 10) == ((guess / 10) % 10) || ((secretNumber / 10) % 10) == (guess % 10)) {
+            ++cows;
+        }
+    }
+}
+
+int main() {
+    srand(time(nullptr));
+    const int maxAttempts = 10;
+    int secretNumber = generateSecretNumber();
+    int attemptCount = 0;
+
+    cout << "Welcome to the Bulls and Cows Game!" << endl;
+    cout << "Try to guess the secret 4-digit number!" << endl;
+
+    while (attemptCount < maxAttempts) {
+        int guess;
+        cout << "Try #" << attemptCount + 1 << ": ";
+
+        while (!(cin >> guess) || guess < 1000 || guess > 9999) {
+            cout << "Invalid attempt! Please enter a 4-digit number: ";
+        }
+
+        int bulls, cows;
+        checkGuess(secretNumber, guess, bulls, cows);
+
+        cout << bulls << " bull";
+        if (bulls != 1) {
+            cout << "s";
+        }
+        cout << " and " << cows << " cow";
+        if (cows != 1) {
+            cout << "s";
+        }
+        cout << "." << endl;
+
+        if (bulls == 4) {
+            cout << "Congratulations! You guessed the secret number!" << endl;
+            break;
+        }
+
+        ++attemptCount;
+    }
+
+    if (attemptCount == maxAttempts) {
+        cout << "Sorry, but you have exhausted all attempts. The secret number was: " << secretNumber << endl;
+    }
+
+    return 0;
+}
